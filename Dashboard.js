16ecb8cb8e40c9677cea2288deeed897eddaa2ef
ishwar Naik <2020bcs130@sggs.ac.in>
2023-11-11 18:28:35 +0530

    delete functionality done , sign in , signup done

--- Dashboard.js
+++ Dashboard.js
@@ -1,11 +1,21 @@
 
-async function fetchData() {
-    try {
-        const response = await fetch('https://fakestoreapi.com/products');
-        let data = await response.json();
-        console.log(data);
-       
-        let table= document.getElementById("Ghayal")
+
+let data ;
+async function deleteRow(id){
+    console.log("deleting: ",id)
+    data.splice(id-1, 1);
+    console.log("data now : ", data)
+    let table= document.getElementById("Ghayal")
+    table.innerHTML= ""
+    table.innerHTML+=`<tr class="bg-primary text-white ">
+    <th>ID</th>
+    <th>Title</th>
+    <th>Description</th>
+    <th>Category</th>
+    <th>Price</th>
+    <th>Actions</th>
+</tr>`
+
         data.map((product, index)=> {
             
             table.innerHTML +=`
@@ -14,7 +24,42 @@ async function fetchData() {
                         <td> ${product.title}</td>
                         <td>${product.description}</td>
                         <td>${product.category}</td>
+                       
                         <td>${product.price}</td>
+                        <td>
+                        <button class="btn-danger btn " onclick='deleteRow(${product.id})'>Delete</button>
+                        </td>
+                    </tr>
+            `
+        })
+}
+
+async function fetchData() {
+    try {
+        let URL = "https://fakestoreapi.com/products"
+        const response = await fetch(URL);
+         data = await response.json();
+        console.log("Data : " , data);
+       
+        let username; 
+        let userobject = JSON.parse(sessionStorage.getItem("user-object"))
+        username= userobject.username
+        document.getElementById("welcome-note").innerText = `Hi, ${username}`
+        let table= document.getElementById("Ghayal")
+
+        data.map((product, index)=> {
+            
+            table.innerHTML +=`
+                    <tr id="tablerows-${product.id}" class="border">
+                        <td>${product.id}</td>
+                        <td> ${product.title}</td>
+                        <td>${product.description}</td>
+                        <td>${product.category}</td>
+                       
+                        <td>${product.price}</td>
+                        <td>
+                        <button class="btn-danger btn " onclick='deleteRow(${product.id})'>Delete</button>
+                        </td>
                     </tr>
             `
         })
---
+++ SignIn.js
@@ -0,0 +1,38 @@
+document.getElementById("login-submit").addEventListener("click", function() {
+    console.log("click:")
+    let username = document.getElementById("username").value ;
+    let password = document.getElementById("password").value ;
+    const USERNAME_LENGTH= 4;
+    const PASSWORD_LENGTH = 6;
+    
+    // let username= "ishwar"
+    // let password = "war"
+    document.getElementById("username-error").style.display="block"
+    document.getElementById("password-error").style.display="block"
+    console.log("username, password : ", username , password)
+    document.getElementById("username-error").innerText=""
+    document.getElementById("password-error").innerText=""
+    if (username.length < USERNAME_LENGTH) {
+        console.log("username length less")
+        // alert("Username should be more than 4 characters")
+        document.getElementById("username").value = ""
+
+        document.getElementById("username-error").style.display="block"
+        document.getElementById("username-error").innerText="Please enter a valid username"
+        return ;
+    }
+    if (password.length < PASSWORD_LENGTH) {
+        // alert("Password should be more than 6 characters")
+        document.getElementById("password").value = ""
+        document.getElementById("password-error").style.display="block"
+        document.getElementById("password-error").innerText="Password length should be more than 6"
+        return ;
+    }
+    let userobject = {
+        username : username, 
+
+        password : password
+    }
+    sessionStorage.setItem("user-object" , JSON.stringify(userobject))
+    window.location.href ="file:///D:/task/jsProject1/jsProject/jsProject/Dashboard.html"
+})
